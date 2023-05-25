160a722a5985365698e8c22779af2022ed1ee7ec
ZhengXianda <xianda.zheng@jiamiantech.com>
2023-05-25 18:32:55 +0800

    优化demo，批量绘制

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -35,18 +35,22 @@ int main()
   world->SetSubStepping(true);
   world->SetDebugDraw(drawer.get());
 
-  b2BodyDef bd;                          // 创建刚体定义对象
-  bd.position.Set(-20.0f, 0.0f);         // 设置初始位置
-  bd.type = b2_dynamicBody;              // 设置为动态刚体
-  b2Body *body = world->CreateBody(&bd); // 创建刚体
-  b2PolygonShape shape;                  // 创建多边形形状
-  shape.SetAsBox(1.0f, 1.0f);            // 设置形状大小
-  b2FixtureDef fd;                       // 创建夹具定义对象
-  fd.shape = &shape;                     // 设置夹具形状
-  fd.density = 1.0f;                     // 设置密度
-  fd.friction = 0.3f;                    // 设置摩擦系数
-  body->CreateFixture(&fd);
-  body->SetLinearVelocity(b2Vec2(1.0f, 0.0f));
+  int count = 20;
+  for (size_t i = 0; i < count; i++)
+  {
+    b2BodyDef bd;                          // 创建刚体定义对象
+    bd.position.Set(-20.0f, 0.0f);         // 设置初始位置
+    bd.type = b2_dynamicBody;              // 设置为动态刚体
+    b2Body *body = world->CreateBody(&bd); // 创建刚体
+    b2PolygonShape shape;                  // 创建多边形形状
+    shape.SetAsBox(1.0f, 1.0f);            // 设置形状大小
+    b2FixtureDef fd;                       // 创建夹具定义对象
+    fd.shape = &shape;                     // 设置夹具形状
+    fd.density = 1.0f;                     // 设置密度
+    fd.friction = 0.3f;                    // 设置摩擦系数
+    body->CreateFixture(&fd);
+    body->SetLinearVelocity(b2Vec2(1.0f, 0.0f));
+  }
 
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
